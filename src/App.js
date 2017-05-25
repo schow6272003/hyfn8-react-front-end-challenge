4a7518f8cb5716d9f1bfade0c272fc34a56919c2
steven.chow <stevenwchow@gmail.com>
2017-05-24 22:29:27 -0700

    challenge completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import Datatable from './Datatable';
-import './App.css';
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Datatable />
+      <div>
+        <div className="row">
+          <div className="col-md-12 col-md-offset-4">
+             <h3>Hyfn8 React Front End Challenge</h3>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+           <Datatable />
+          </div>
+        </div>
+       
       </div>
     );
   }
